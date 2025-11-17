6204fa34cf8295486dbead8de573cde979743b23
fionadark <brigiddark@gmail.com>
2025-11-17 11:33:17 -0800

    set up spring boot application

---
+++ src/main/java/thelittlelibrary/LittleLibrary.java
@@ -0,0 +1,12 @@
+package thelittlelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleLibrary {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(LittleLibrary.class, args);
+    }
+}
