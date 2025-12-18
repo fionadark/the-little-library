6532a68fc6462b91af9a582881eff3aa35a1b880
fionadark <brigiddark@gmail.com>
2025-12-17 20:56:27 -0800

    .

--- src/main/java/thelittlelibrary/controller/BookSearchController.java
+++ src/main/java/thelittlelibrary/controller/BookSearchController.java
@@ -11,10 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * REST controller for book search operations.
- * Provides search functionality using OpenLibrary API.
- */
+
 @RestController
 @RequestMapping("/search")
 public class BookSearchController {
